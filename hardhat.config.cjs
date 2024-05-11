818368cc94cdac3962aac14d95f5c6a03b23fed5
yashj09 <yashj8858@gmail.com>
2024-05-12 04:53:24 +0530

    deploy

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -5,6 +5,13 @@ dotenv.config();
 module.exports = {
   solidity: "0.8.24",
   networks: {
-    url: process.env.SEPOLIA_RPC_URL,
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
+
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
   }
 };
