c849e37fdc19b73d7eeaf7cee255927cd1d62165
yashj09 <yashj8858@gmail.com>
2024-05-12 04:50:05 +0530

    env

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,6 +1,10 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+const dotenv = require('dotenv');
+dotenv.config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    url: process.env.SEPOLIA_RPC_URL,
+  }
 };
--- scripts/deployNarutoNFT.js
+++ scripts/deployNarutoNFT.js
@@ -2,7 +2,7 @@ const hre = require("hardhat");
 
 async function main() {
   const NarutoNFT = await hre.ethers.getContractFactory("NarutoNFT");
-  const narutoNFT = await NarutoNFT.deploy();
+  const narutoNFT = await NarutoNFT.deploy(0xb677DB21Fd004fD058AA2E06cEee24b65F9c91F4);
   await narutoNFT.deployed();
   console.log("narutoNFT deployed to:", narutoNFT.address);
 }
