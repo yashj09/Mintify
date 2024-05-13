dc900e14c52f03302fbd6ca6341c2d0a09c1b435
yashj09 <yashj8858@gmail.com>
2024-05-13 22:20:40 +0530

    verify contract

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-etherscan");
 const dotenv = require('dotenv');
 dotenv.config();
 /** @type import('hardhat/config').HardhatUserConfig */
